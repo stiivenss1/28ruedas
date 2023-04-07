98449622d6f7509c3b59e5c70362d7c522c538ef
Stevent Florez <steventflorez94@gmail.com>
2023-04-07 14:12:47 +0200

    se agrega carpeta y datos de firebase

---
+++ firebase/data.js
@@ -0,0 +1,95 @@
+let traer_btn = document.getElementById("traer");
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Listen for form submissions
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get form values
+  var titulo = document.querySelector("#titulo").value;
+  var precio = document.querySelector("#precio").value;
+  var descripcion = document.querySelector("#descripcion").value;
+
+  // GUARDAR SERVICIOS
+  database.ref("servicios").push({
+    titulo,
+    precio,
+    descripcion,
+  });
+
+  // Clear form fields
+  document.querySelector("#titulo").value = "";
+  document.querySelector("#precio").value = "";
+  document.querySelector("#descripcion").value = "";
+
+  // Show success message
+  alert("Mensaje enviado correctamente!");
+
+  
+});
+
+
+traer_btn.addEventListener('click', ()=>{
+    database.ref("servicios/").remove().then(()=>{
+        alert('eliminado')
+    })
+    .catch((error) => {
+        console.error("Error al eliminar el registro: ", error);
+      });
+    
+    
+})
+
+
+
+
+/**CREAR SERVICIO */
+async function  guardarServicios (servicio,precio,descripcion) {
+    try {
+        await database.ref("servicios").push({
+            servicio,
+            precio,
+            descripcion,
+          });
+        
+    } catch (error) {
+        console.log(error)
+    }
+   
+    
+}
+/**LLAMAR TODOS LOS SERVICIOS */
+
+async function  llamarServicios () {
+
+
+    await database.ref("servicios").get().then((querySnapshot) => {
+            
+           return querySnapshot.val()
+    
+    })
+    .catch((error) => {
+        alert.error("Error al obtener los servicios: ", error);
+    });
+    
+    
+}
+
+//ELIMINAR SERVICIO
+async function  eliminarServicio (id) {
+
+
+    await  database.ref("servicios/"+id).remove().then(()=>{
+        alert('eliminado')
+    })
+    .catch((error) => {
+        console.error("Error al eliminar el registro: ", error);
+      });
+    
+    
+}
+
+
+
